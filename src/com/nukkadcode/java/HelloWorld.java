c589ae9e2c791d012b76c30749f0d260b53520bb
nukkadcode <nukkadcode@gmail.com>
2018-07-18 18:39:45 +0530

    automatic promotion example

---
+++ src/com/nukkadcode/java/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.nukkadcode.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		p('A');
+		
+
+	}
+	
+/*	public static void p(char d) {
+		System.out.println(d);
+		
+		System.out.println("Surprise! our character 'A' is char");
+	}*/
+	
+/*	public static void p(int d) {
+		System.out.println(d);
+
+		System.out.println("Surprise! our character 'A' has become now an int");
+	}
+	*/
+/*	public static void p(float d) {
+		System.out.println(d);
+
+		System.out.println("Surprise! our character 'A' has become now a float");
+	}
+	*/
+	public static void p(double d) {
+		System.out.println(d);
+
+		System.out.println("Surprise! our character 'A' has become now a double");
+	}
+	
+}
